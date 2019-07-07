ae4b7ab920e120256a7e95f4ff046b61f220bdbd
s.shulzhytskaya <sshulzhitskaya@xbsoftware.com>
2019-07-07 23:46:47 +0300

    [add] user view

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -1,2 +1,23 @@
 package ru.skillbranch.devintensive.extensions
 
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+
+fun User.toUserView(): UserView {
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils?.toInitials(firstName, lastName)?:""
+    var time = lastVisit
+    val status = when {
+        lastVisit == null -> "Еще ниразу не было"
+        isOnline -> "online"
+        else -> "последний раз был ${time?.humanizeDiff()}"
+    }
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        avatar = avatar,
+        nickName = nickName,
+        initials = initials,
+        status = status)
+}
--- app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
@@ -1,2 +1,22 @@
 package ru.skillbranch.devintensive.models
 
+class UserView(
+    val id: String,
+    val fullName: String,
+    val nickName: String,
+    val initials: String,
+    val avatar: String? = null,
+    val status: String? = "offline"
+){
+    fun printMe(){
+        println("""
+            USERVIEW:
+            id: $id,
+            fullName: $fullName,
+            nickName: $nickName,
+            initials: $initials,
+            avatar: $avatar,
+            status: $status
+        """.trimIndent())
+    }
+}
