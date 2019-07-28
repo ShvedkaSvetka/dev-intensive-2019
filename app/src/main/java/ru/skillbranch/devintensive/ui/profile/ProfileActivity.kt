22ccbbbe75e4dadfe0cf53beef918730cdf5f7e4
s.shulzhytskaya <sshulzhitskaya@xbsoftware.com>
2019-07-29 00:16:43 +0300

    init: constraint view

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -10,7 +10,7 @@ class ProfileActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_profile_constraint)
         Log.d("M_MainActivity.kt", "onCreate")
     }
 
