aae5d74000aeab6ac09ffcc75f350a076a5a3d7e
s.shulzhytskaya <sshulzhitskaya@xbsoftware.com>
2019-08-05 23:14:45 +0300

    [fix] save method

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -148,7 +148,7 @@ class ProfileActivity : AppCompatActivity() {
         Profile(
             firstName = et_first_name.text.toString(),
             lastName = et_last_name.text.toString(),
-            about = et_last_name.text.toString(),
+            about = et_about.text.toString(),
             repository = et_repository.text.toString()
         ).apply {
             viewModel.saveProfileDate(this )
